0b580a78bab7323460681666d36b607b9379e61f
Dinesh <dinesh25195@gmail.com>
2020-09-21 22:36:07 +0530

    allow cors

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,16 @@
 import axios from 'axios';
 const API_URI = "https://my-own-calculator.herokuapp.com/api/calculations";
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/json',
+};
+
 export const GET_ALL = () => {
   return axios
-    .get(API_URI)
+    .get(API_URI, {
+      headers
+    })
     .then(response => {
       return response.data;
    })
@@ -18,6 +25,8 @@ export const CREATE_ONE = (id, input, output) => {
         id,
         input,
         output
+    }, {
+      headers
     })
     .then(response => {
       return response.data;
@@ -29,7 +38,9 @@ export const CREATE_ONE = (id, input, output) => {
 
 export const DELETE_ALL = () => {
   return axios
-    .delete(API_URI)
+    .delete(API_URI, {
+      headers
+    })
     .then(response => {
       return response.data;
     })
